5194b59b22004bac02b63c72be47481a8b85b2dd
HemanthPoojari <hemanth123.@gmail.com>
2022-04-28 21:25:59 +0530

    hdhdh

---
+++ DAO/TvChannelDAO.java
@@ -0,0 +1,10 @@
+package com.xworkz.tvchannel.DAO;
+
+import com.xworkz.tvchannel.entity.TvChannel;
+
+public interface TvChannelDAO {
+		public void saveChannel(TvChannel channel);
+		public void getChannel();
+	}
+
+
